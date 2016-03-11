043fff86b3038c0a00f42924c8b80a76cae13980
james@jamespamplin.com <james@jamespamplin.com>
2016-03-11 18:14:10 +0000

    Use UrlBuilder to build urls with params for errors

--- app/com/gu/identity/frontend/models/UrlBuilder.scala
+++ app/com/gu/identity/frontend/models/UrlBuilder.scala
@@ -1,12 +1,16 @@
 package com.gu.identity.frontend.models
 
+import com.gu.identity.frontend.errors.AppException
 import play.api.mvc.Call
 
 object UrlBuilder {
 
+  type UrlParameter = (String, String)
+  type UrlParameters = Seq[UrlParameter]
+
   private def encode = java.net.URLEncoder.encode(_: String, "UTF8")
 
-  def apply(baseUrl: String, params: Seq[(String, String)]): String =
+  def apply(baseUrl: String, params: UrlParameters): String =
     params.headOption match {
       case None => baseUrl
       case _ => {
@@ -25,10 +29,15 @@ object UrlBuilder {
     apply(baseUrl, returnUrl, skipConfirmation = None, clientId)
 
   def apply(baseUrl: String, returnUrl: ReturnUrl, skipConfirmation: Option[Boolean], clientId: Option[ClientID]): String =
-    apply(baseUrl, buildParams(returnUrl, skipConfirmation, clientId))
+    apply(baseUrl, buildParams(Some(returnUrl), skipConfirmation, clientId))
 
+  def apply(baseUrl: String, returnUrl: ReturnUrl, skipConfirmation: Option[Boolean], clientId: Option[ClientID], error: AppException): String =
+    apply(baseUrl, buildParams(Some(returnUrl), skipConfirmation, clientId, Some(error)))
 
-  def apply(call: Call, params: Seq[(String, String)]): String =
+  def apply(baseUrl: String, error: AppException): String =
+    apply(baseUrl, buildParams(error = Some(error)))
+
+  def apply(call: Call, params: UrlParameters): String =
     apply(call.url, params)
 
   def apply(call: Call, returnUrl: ReturnUrl, clientId: Option[ClientID]): String =
@@ -38,10 +47,15 @@ object UrlBuilder {
     apply(call.url, returnUrl, skipConfirmation, clientId)
 
 
-  private def buildParams(returnUrl: ReturnUrl, skipConfirmation: Option[Boolean], clientId: Option[ClientID]): Seq[(String, String)] =
+  private def buildParams(
+      returnUrl: Option[ReturnUrl] = None,
+      skipConfirmation: Option[Boolean] = None,
+      clientId: Option[ClientID] = None,
+      error: Option[AppException] = None): UrlParameters =
     Seq(
-      Some(returnUrl).filterNot(_.isDefault).map("returnUrl" -> _.url),
+      returnUrl.flatMap(_.toStringOpt).map("returnUrl" -> _),
       skipConfirmation.map("skipConfirmation" -> _.toString),
-      clientId.map("clientId" -> _.id)
+      clientId.map("clientId" -> _.id),
+      error.map("error" -> _.id.key)
     ).flatten
 }
